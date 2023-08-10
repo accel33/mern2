f94b72ef431d070873b7533b07e99e7ab196e1b1
accel33 <accelwtf@gmail.com>
2023-08-10 17:23:18 -0500

    md

--- markdown/Pasos.md
+++ markdown/Pasos.md
@@ -2,3 +2,6 @@
 
 1. Configuracion de package.json, express, nodemon, gitignore.
 2. Creamos ruta estatica para /public
+3. Utilizamos el metodo .join() de "path" para no tener inconvenientes con las rutas de distintos sistemas operativos
+4. Creamos las plantillas html en /views
+5. Creamos las rutas de acceso HTTP en /routes
--- servidor.js
+++ servidor.js
@@ -1,27 +1,25 @@
-const express = require('express')
-const app = express()
-const path = require('path');
-const PORT = process.env.PORT || 3000
+const express = require("express");
+const app = express();
+const path = require("path");
+const PORT = process.env.PORT || 3000;
 
-const ruta = path.join(__dirname, '/public')
-const ruta404 = path.join(__dirname, 'views', '404.html')
+const ruta = path.join(__dirname, "/public");
+const ruta404 = path.join(__dirname, "views", "404.html");
 
-app.use('/', express.static(path.join(__dirname, '/public')))
-app.use('/', require('./routes/root'))
+app.use("/", express.static(ruta));
+app.use("/", require("./routes/root"));
 console.log(ruta404);
-app.all('*', (req, res) => {
-  res.status(404)
-  if (req.accepts('html')) {
-    res.sendFile(path.join(__dirname, 'views', '404.html'))
-  } else if (req.accepts('json')) {
-    res.json({ message: '404 No Encontraado' })
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(ruta404);
+  } else if (req.accepts("json")) {
+    res.json({ message: "404 No Encontraado" });
   } else {
-    res.type('txt').send('404 No Encontrado')
+    res.type("txt").send("404 No Encontrado");
   }
-})
+});
 
 app.listen(PORT, () => {
   console.log(`Escuchando en el puerto ${PORT}`);
-})
-
-
+});
